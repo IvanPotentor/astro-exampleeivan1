5766c22c64d4cf5baf7619574abbfc0da6f0793c
IvanPotentor <109097511+IvanPotentor@users.noreply.github.com>
2023-10-25 17:24:45 -0600

    Prueba 1 astro js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animated'),
+    require("tw-elements/dist/plugin")
+  ],
   darkMode: ['class', '.darkmode'],
 }
