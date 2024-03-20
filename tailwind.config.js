53efc275e60355b6038f8ce330fd62383d6f9c0e
IvanPotentor <jespinosa@potentor.com.mx>
2024-03-20 00:00:51 -0600

    hola

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,12 @@
 module.exports = {
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-    "./node_modules/tw-elements/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('tailwindcss-animated'),
-    require("tw-elements/plugin.cjs"),
     require('flowbite/plugin')
   ],
   darkMode: ['class', '.darkmode'],
